9397df620c6cc1cf471f86fff2feb75b17597cdb
Alfi74 <catwyzle@ymail.com>
2023-08-18 17:56:23 +0200

    Neue Variablen

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// Abfrage neue ToDoListe
+
+const contentNewToDo = document.getElementById('newToDo').value;
+
+// CheckBox neue Liste
+
+const checkMark = document.getElementsByClassName('form-check-input');
+
+// Mülleimer
+
+const trash = document.getElementById('be-trash');
+
+// Button Content newToDo
+
+const btn = document.getElementsByClassName('btn');
+
+// Section ToDos als Variable
+
+const section = document.getElementById('toDos');
+
+// 
+
+
